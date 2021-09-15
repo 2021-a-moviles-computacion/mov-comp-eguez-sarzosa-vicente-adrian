ae488694cc867af677327fd3b048c2006355ed5f
Adrian <eadepto@hotmail.com>
2021-09-15 12:40:34 -0500

    mapas

--- 03-firebase/app/src/main/java/com/example/firebaseuno/FMapsActivity.kt
+++ 03-firebase/app/src/main/java/com/example/firebaseuno/FMapsActivity.kt
@@ -3,10 +3,17 @@ package com.example.firebaseuno
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class FMapsActivity : AppCompatActivity() {
 
@@ -16,15 +23,108 @@ class FMapsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fmaps)
         solicitarPermisos()
+
+
+        val botonCarolina = findViewById<Button>(R.id.btn_ir_carolina)
+        botonCarolina
+            .setOnClickListener {
+                val carolina = LatLng(-0.18288452555103193, -78.48449971346241)
+                val zoom = 17f
+                moverCamaraConZoom(carolina, zoom)
+            }
+
         val fragmentoMapa = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         fragmentoMapa.getMapAsync { googleMap ->
             if(googleMap != null){
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                val quicentro = LatLng(-0.176125, -78.480208)
+                val titulo = "Quicentro"
+                val zoom = 17f
+                anadirMarcador(quicentro, titulo)
+                moverCamaraConZoom(quicentro, zoom)
+                // LINEA
+                val poliLineaUno = googleMap
+                    .addPolyline(
+                        PolylineOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1759187040647396, -78.48506472421384),
+                                LatLng(-0.17632468492901104, -78.48265589308046),
+                                LatLng(-0.17746143130181483, -78.4770533307815)
+                            )
+                    )
+                poliLineaUno.tag = "linea-1" // <- ID
+
+                // POLIGONO
+                val poligonoUno = googleMap
+                    .addPolygon(
+                        PolygonOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1771546902239471, -78.48344981495214),
+                                LatLng(-0.17968981486125768, -78.48269198043828),
+                                LatLng(-0.17710958124147777, -78.48142892291516)
+                            )
+                    )
+                poligonoUno.fillColor = -0xc771c4
+                poligonoUno.tag = "poligono-2" // <- ID
+                escucharListeners()
             }
         }
     }
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+        }
+
+        mapa
+            .setOnMarkerClickListener {
+                Log.i("mapa", "setOnMarkerClickListener ${it}")
+                return@setOnMarkerClickListener true
+            }
+        mapa
+            .setOnCameraMoveListener {
+                Log.i("mapa", "setOnCameraMoveListener")
+            }
+        mapa
+            .setOnCameraMoveStartedListener {
+                Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+            }
+
+        mapa
+            .setOnCameraIdleListener {
+                Log.i("mapa", "setOnCameraIdleListener")
+            }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String){
+        mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )
+    }
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
+    }
+
+
+
+
+
+
+
+
     fun solicitarPermisos(){
         val contexto = this.applicationContext
         val permisosFineLocation = ContextCompat
